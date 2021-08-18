4031a9d08b6561de6cc4f7a043c353f7f6d1a0bc
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-08-18 12:43:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+function insert(num) {
+    var number = document.getElementById("result").innerHTML;
+    document.getElementById("result").innerHTML = number + num;
+}
+
+function clean() {
+    document.getElementById("result").innerHTML = " ";
+}
+
+function back() {
+    var result = document.getElementById("result").innerHTML;
+    document.getElementById("result").innerHTML = result.substring(0, result.length
+        - 1);
+}
+
+function calcular() {
+    var result = document.getElementById("result").innerHTML;
+    if (result) {
+        document.getElementById("result").innerHTML = eval(result);
+    }
+    else { document.getElementById("result").innerHTML = "NoData"; }
+
+}
+
+
+function vibrar() {
+    navigator.vibrate(25);
+}
+
+window.navigator = window.navigator || {};
+document.getElementById('cle').addEventListener('click', vibrar);
+document.getElementById('bac').addEventListener('click', vibrar);
+document.getElementById('div').addEventListener('click', vibrar);
+document.getElementById('mul').addEventListener('click', vibrar);
+document.getElementById('seven').addEventListener('click', vibrar);
+document.getElementById('eight').addEventListener('click', vibrar);
+document.getElementById('nine').addEventListener('click', vibrar);
+document.getElementById('add').addEventListener('click', vibrar);
+document.getElementById('four').addEventListener('click', vibrar);
+document.getElementById('five').addEventListener('click', vibrar);
+document.getElementById('six').addEventListener('click', vibrar);
+document.getElementById('subt').addEventListener('click', vibrar);
+document.getElementById('one').addEventListener('click', vibrar);
+document.getElementById('two').addEventListener('click', vibrar);
+document.getElementById('three').addEventListener('click', vibrar);
+document.getElementById('equal').addEventListener('click', vibrar);
+document.getElementById('zero').addEventListener('click', vibrar);
+document.getElementById('dot').addEventListener('click', vibrar);
